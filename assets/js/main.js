8b103483f2944b52d2c91fa1b6e8dcd7946eb307
THNisme <nghiath.ce191115@gmail.com>
2025-01-31 08:25:23 +0700

    preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,50 @@ const $$ = document.querySelectorAll.bind(document);
 const musicBtn = $('.music-box-btn');
 const musicBox = $('.music-box');
 
+musicBox.controls = true;
+
+const preloader = document.getElementById("preloader");
+const playButton = document.getElementById("playButton");
+
 const app = {
 
+    preLoader: function () {
+        window.addEventListener("load", function () {
+            // Khi trang load xong, hiện nút Play
+            setTimeout(() => {
+                playButton.style.display = "block";
+            }, 2000);
 
-    handleEvent: function() {
+            // Khi user bấm Play, ẩn preloader
+            playButton.addEventListener("click", function () {
+                musicBox.play();
+                preloader.style.opacity = "0";
+                setTimeout(() => {
+                    preloader.style.display = "none";
+                    document.body.style.overflow = "auto"; // Cho phép cuộn lại
+                }, 300); // Hiệu ứng ẩn dần
+            });
+        });
 
-        musicBox.autoplay = true;
-        musicBox.controls = true;
+
+    },
+
+    handleEvent: function () {
 
         musicBtn.onclick = function () {
             if (musicBox.classList.contains('inactive')) {
                 musicBox.classList.remove('inactive');
                 musicBox.classList.add('active');
-            } else  {
+            } else {
                 musicBox.classList.remove('active');
                 musicBox.classList.add('inactive');
             }
         }
     },
-       
 
-    start: function() {
-        musicBox.play();
+
+    start: function () {
+        this.preLoader();
         this.handleEvent();
     }
 }
